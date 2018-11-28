d2b9b5b84a1341a2db303aeb92cff8338c1658fe
mannysin <sinclair2610@gmail.com>
2018-11-28 15:05:12 -0500

    deployment db

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const flash         = require("connect-flash");
 const config        = require('./config/passport');
 
 mongoose
-  .connect('mongodb://localhost/ironGR', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
